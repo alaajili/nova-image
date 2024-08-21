76e1dd3796c475b5749b5ee1ffa8c211ca815215
alaajili <abderrahmanelaajili0@gmail.com>
2024-08-21 21:24:16 +0100

    fix clerk webhooks error

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,9 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 const isProtectedRoute = createRouteMatcher(['/profile(.*)'])
 
+
 export default clerkMiddleware((auth, req) => {
+
   if (isProtectedRoute(req)) auth().protect()
 })
 
